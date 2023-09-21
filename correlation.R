ba73e373889bece428581aa25b878ed6f0641915
mdabarik <mdabarik19@gmail.com>
2023-09-21 15:25:06 +0600

    intercept added

--- correlation.R
+++ correlation.R
@@ -8,6 +8,7 @@ find_corr <- function(X, Y) {
 
 # implementation
 compute_correlation <- function(X, Y) {
+  res <- 0 #index1: correlation, index2: coefficient, index3: intercept
   n = length(X)
   sumXY = 0
   sumX = 0
@@ -15,19 +16,35 @@ compute_correlation <- function(X, Y) {
   sumXSquare = 0
   sumYSquare = 0
   for (i in 1:length(X)) {
-    sumXY = sumXY + (X[i] *  Y[i])
     sumX = sumX + X[i];
     sumY = sumY + Y[i];
+    sumXY = sumXY + (X[i] *  Y[i])
     sumXSquare = sumXSquare + (X[i] * X[i])
     sumYSquare = sumYSquare + (Y[i] * Y[i])
   }
   lob <- sumXY - ((sumX * sumY) / n) # hor
   hor <- sqrt((sumXSquare - ((sumX * sumX) / n)) * (sumYSquare - ((sumY * sumY) / n))) # lob
-  return (lob / hor)
+  corr <- lob / hor
+  coef <- lob / ((n * sumXSquare) - sumX*sumX)
+  incept <- (sumY / n) - (coef * (sumX/n))
+  res[1] = corr;
+  res[2] = coef;
+  res[3] = incept
+  return (res)
 }
 
 
 H <- c(67, 65, 68, 62, 61)
 W <- c(60, 62, 63, 56, 50)
 find_corr(H, W)
-compute_correlation(H, W)
+res <- compute_correlation(H, W)
+res[1]
+res[2]
+res[3]
+
+
+
+
+
+
+
