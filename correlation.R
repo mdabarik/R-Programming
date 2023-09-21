028f2f0ef3bed8a2e032c1980a1bde801c5a0c65
mdabarik <mdabarik19@gmail.com>
2023-09-21 15:09:58 +0600

    correlation

---
+++ correlation.R
@@ -0,0 +1,33 @@
+
+
+
+# built in function
+find_corr <- function(X, Y) {
+  return (cor(X, Y))
+}
+
+# implementation
+compute_correlation <- function(X, Y) {
+  n = length(X)
+  sumXY = 0
+  sumX = 0
+  sumY = 0
+  sumXSquare = 0
+  sumYSquare = 0
+  for (i in 1:length(X)) {
+    sumXY = sumXY + (X[i] *  Y[i])
+    sumX = sumX + X[i];
+    sumY = sumY + Y[i];
+    sumXSquare = sumXSquare + (X[i] * X[i])
+    sumYSquare = sumYSquare + (Y[i] * Y[i])
+  }
+  lob <- sumXY - ((sumX * sumY) / n)
+  hor <- sqrt((sumXSquare - ((sumX * sumX) / n)) * (sumYSquare - ((sumY * sumY) / n)))
+  return (lob / hor)
+}
+
+
+H <- c(67, 65, 68, 62, 61)
+W <- c(60, 62, 63, 56, 50)
+find_corr(H, W)
+compute_correlation(H, W)
