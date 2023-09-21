79ea79ac0cb16f633a75c474693aa0bc7134561a
mdabarik <mdabarik19@gmail.com>
2023-09-21 15:11:37 +0600

    correlation added

--- sep-21-class.R
+++ sep-21-class.R
@@ -5,8 +5,13 @@ blood_sugar <- c(110, 118, 130, 140, 142, 146, 112, 100, 95, 98)
 
 # Line Chart Drawing
 x <- 1:length(blood_sugar)
-plot(x, blood_sugar, type = "o", pch = 19, ylim = c(0, max(blood_sugar) + 10), 
-     xlab = "Measurement", ylab = "Blood Sugar")
+plot(
+  x=x, 
+  y=blood_sugar, 
+  type = "o", 
+  pch = 19, 
+  ylim=c(0,150),
+  xlab = "Measurement", ylab = "Blood Sugar")
 lines(x, blood_sugar, type = "o", pch = 19)
 grid()
 title("Blood Sugar Over Time")
@@ -20,6 +25,10 @@ t_test_result$p.value
 # Print the result
 print(t_test_result)
 
+
+#sum(xy)- sum(x) * sum(y) 
+#............................
+#sqrt (sum(x^2) - sum(x)^2) * 
 
 
 
